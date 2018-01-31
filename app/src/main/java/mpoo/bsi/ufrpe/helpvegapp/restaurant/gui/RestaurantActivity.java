0b3ede99eddefb244f7dab345a4f3def872c5518
Isabel Santiago <mariaisabelcs@live.com>
2018-01-31 17:44:23 -0200

    Inicio da criacao da tela de perfil restaurante

---
+++ app/src/main/java/mpoo/bsi/ufrpe/helpvegapp/restaurant/gui/RestaurantActivity.java
@@ -0,0 +1,14 @@
+package mpoo.bsi.ufrpe.helpvegapp.restaurant.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import mpoo.bsi.ufrpe.helpvegapp.R;
+
+public class RestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+    }
+}
